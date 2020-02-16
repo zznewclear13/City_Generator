d9db0572c0076cdce869a8196831e44b802d3875
zznewclear13 <zznewclear13@users.noreply.github.com>
2020-02-16 00:08:42 -0800

    improve random points

--- randomScatter.py
+++ randomScatter.py
@@ -4,6 +4,7 @@ import cv2
 import math
 import random
 import bisect
+from scipy.spatial import Voronoi, voronoi_plot_2d
 
 from itertools import accumulate
 
@@ -18,6 +19,8 @@ class randomScatter():
         self.position = []
         self.row = []
         self.col = []
+        self.points = []
+        self.improvedPoints = []
 
     def plotRandomMat(self, shape, ax):
         x = np.random.rand(shape[0])
@@ -61,17 +64,42 @@ class randomScatter():
         index = bisect.bisect_right(inputList, randomNumber)
         return index
 
-    def drawScatter(self, alpha):
-        self.ax = plt.scatter(self.col, self.row, alpha=alpha)
-        plt.show()    
+    def makePoints(self):
+        self.points = [list(pair) for pair in zip(self.row, self.col)]
+        return self.points
+
+    def improvePoints(self, points):
+        self.improvedPoints = []
+        vor = Voronoi(points)
+        for region in vor.regions:
+            if not -1 in region:
+                regionPoints = np.array([vor.vertices[i] for i in region])
+                np.sum(regionPoints, axis=0)
+                if list(regionPoints.shape)[0] != 0:
+                    newPosition = list(np.sum(regionPoints, axis=0)/list(regionPoints.shape)[0])
+                    if newPosition[0] >=-self.shape[0] and newPosition[0] <= self.shape[0]*2 and newPosition[1] >=-self.shape[1] and newPosition[1] <= self.shape[1]*2:
+                        self.improvedPoints += [newPosition]    
+        return self.improvedPoints        
+
+    def drawScatter(self, points, alpha, color='#000000'):
+        points = np.array(points)
+        self.ax = plt.scatter(points[:,1], points[:,0], alpha=alpha, c=color) #flip the diagram vertically
+  
 
 ERROR_MESSAGE = '----------------------'
 
 if __name__ == "__main__":
     IMAGE_INPUT_PATH = './image_input/scatter_rate.jpg'
 
-    randomScatterInstance = randomScatter(IMAGE_INPUT_PATH, 20000, 0, reverse=True, squared=True)
+    randomScatterInstance = randomScatter(IMAGE_INPUT_PATH, 200, 0, reverse=True, squared=True)
     randomScatterInstance.readImage()
     randomScatterInstance.randomDots()
+    randomScatterInstance.makePoints()
     randomScatterInstance.initialise_figure()
-    randomScatterInstance.drawScatter(0.1)
+
+    #run improvePoints twice to refine points' locations
+    randomScatterInstance.improvePoints(randomScatterInstance.points)
+    newPoints = randomScatterInstance.improvePoints(randomScatterInstance.improvedPoints) * 1
+
+    randomScatterInstance.drawScatter(randomScatterInstance.improvedPoints, 0.25, '#ff0000')
+    plt.show()
